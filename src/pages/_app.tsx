838281c544c39731ede6f2f5d5908c71798b7f20
John Gulbronson <john@benzinga.com>
2025-07-09 17:39:54 -0400

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/next"
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@/styles/theme";
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
